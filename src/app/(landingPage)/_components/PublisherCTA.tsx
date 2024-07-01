99544b6b26f22ac7d777c82d6bee3d3623776737
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-07-01 17:19:18 +0500

    Minor Changes

--- src/app/(landingPage)/_components/PublisherCTA.tsx
+++ src/app/(landingPage)/_components/PublisherCTA.tsx
@@ -5,7 +5,7 @@ import { Boxes } from "@/components/ui/background-boxes";
 
 export const PublisherCTA = () => {
   return (
-    <div className="w-full relative mt-24 md:mt-36 px-3 sm:px-6 lg:px-12 py-12 md:py-24 bg-slate-900 overflow-hidden flex md:flex-row flex-col max-md:items-center md:justify-center gap-6 md:gap-12 lg:gap-24">
+    <div className="w-full relative mt-24 md:mt-36 px-3 sm:px-6 lg:px-12 py-16 md:py-24 bg-slate-900 overflow-hidden flex md:flex-row flex-col max-md:items-center md:justify-center gap-6 md:gap-12 lg:gap-24">
       <Boxes />
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
