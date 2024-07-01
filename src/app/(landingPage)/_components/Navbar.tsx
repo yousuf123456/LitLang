395c49cd5cc1c4395564c75e7c036216cfd75a6b
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-07-01 21:12:13 +0500

    Contact Page Added

--- src/app/(landingPage)/_components/Navbar.tsx
+++ src/app/(landingPage)/_components/Navbar.tsx
@@ -13,29 +13,31 @@ export const Navbar = () => {
   if (pathname === "/blogEditor") return;
 
   return (
-    <MaxWidthWrapper className="md:py-6 py-4 lg:px-16 xl:px-20 md:px-8 px-3 border-b border-zinc-300 sticky top-0 left-0 bg-white z-50">
-      <div className="flex justify-between items-center w-full gap-3 sm:gap-8">
-        <div className="w-full flex items-center gap-3 sm:gap-6 lg:gap-12">
-          <div className="flex items-center gap-4">
-            <div className="relative w-7 h-7 sm:w-9 sm:h-9">
-              <Image
-                alt="Company Logo"
-                src={"/logo.svg"}
-                className="object-cover"
-                fill
-              />
+    <div className="border-b border-zinc-200">
+      <MaxWidthWrapper className="md:py-6 py-4 lg:px-16 xl:px-20 md:px-8 px-3 sticky top-0 left-0 bg-white z-50">
+        <div className="flex justify-between items-center w-full gap-3 sm:gap-8">
+          <div className="w-full flex items-center gap-3 sm:gap-6 lg:gap-12">
+            <div className="flex items-center gap-4">
+              <div className="relative w-7 h-7 sm:w-9 sm:h-9">
+                <Image
+                  alt="Company Logo"
+                  src={"/logo.svg"}
+                  className="object-cover"
+                  fill
+                />
+              </div>
+
+              <p className="text-2xl font-semibold text-primary hidden lg:block">
+                LitLang
+              </p>
             </div>
 
-            <p className="text-2xl font-semibold text-primary hidden lg:block">
-              LitLang
-            </p>
+            <Search />
           </div>
 
-          <Search />
+          <UserAccount />
         </div>
-
-        <UserAccount />
-      </div>
-    </MaxWidthWrapper>
+      </MaxWidthWrapper>
+    </div>
   );
 };
