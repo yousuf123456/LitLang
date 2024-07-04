0356144c60acc45b390c21fa18aee3eb84d5c5b6
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-07-04 22:15:02 +0500

    Animation Changed

---
+++ src/app/(landingPage)/_components/AnimatedHeading.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { m, LazyMotion } from "framer-motion";
+const loadFeatures = () =>
+  import("@/app/utils/features").then((res) => res.default);
+
+export const AnimatedHeading = ({ text }: { text: string }) => {
+  return (
+    <LazyMotion strict features={loadFeatures}>
+      <m.h1
+        initial={{ top: 80, opacity: 0 }}
+        animate={{
+          top: 0,
+          opacity: 1,
+          transition: { type: "spring", stiffness: 150, delay: 0.2 },
+        }}
+        className="relative text-3xl min-[420px]:text-4xl md:text-5xl lg:text-6xl font-bold text-center font-primary max-w-lg md:max-w-3xl lg:max-w-4xl capitalize text-gray-800"
+      >
+        {text}
+      </m.h1>
+    </LazyMotion>
+  );
+};
