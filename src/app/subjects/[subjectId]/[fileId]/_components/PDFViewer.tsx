183060a7e9f94f7b3571bb6d99e20c4b08774e87
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-07-13 19:44:40 +0500

    Trying to fix compiling error

---
+++ src/app/subjects/[subjectId]/[fileId]/_components/PDFViewer.tsx
@@ -0,0 +1,216 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import {
+  ChevronLeft,
+  ChevronRight,
+  Loader2,
+  RotateCcw,
+  ZoomIn,
+  ZoomOut,
+} from "lucide-react";
+
+import React, { useEffect, useMemo, useState } from "react";
+
+import { toast } from "sonner";
+
+import { useResizeDetector } from "react-resize-detector";
+
+import { Document, Page, pdfjs } from "react-pdf";
+
+import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
+if (typeof Promise.withResolvers === "undefined") {
+  if (typeof window !== undefined) {
+    // @ts-expect-error This does not exist outside of polyfill which this is doing
+    window.Promise.withResolvers = function () {
+      let resolve, reject;
+      const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
+  } else {
+    // @ts-expect-error This does not exist outside of polyfill which this is doing
+    global.Promise.withResolvers = function () {
+      let resolve, reject;
+      const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
+  }
+}
+
+// there is your `/legacy/build/pdf.worker.min.mjs` url
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+export const PDFViewer = ({
+  uint8ArrayData,
+}: {
+  uint8ArrayData: Uint8Array;
+}) => {
+  const file = useMemo(() => {
+    return { data: uint8ArrayData };
+  }, [uint8ArrayData]);
+
+  const { ref, width } = useResizeDetector();
+
+  const [scale, setScale] = useState(1);
+  const [rotation, setRotation] = useState(0);
+  const [numPages, setNumPages] = useState(0);
+
+  const [pageNumber, setPageNumber] = useState(1);
+  const [inputPageNumber, setInputPageNumber] = useState(1);
+
+  useEffect(() => {
+    if (Number.isNaN(inputPageNumber)) return;
+    if (!numPages) return;
+    if (inputPageNumber === pageNumber) return;
+    if (inputPageNumber < 1 || inputPageNumber > numPages) return;
+
+    setPageNumber(inputPageNumber);
+  }, [inputPageNumber]);
+
+  const goToNextPage = () =>
+    setPageNumber((prev) => {
+      if (prev + 1 > numPages) return prev;
+      return prev + 1;
+    });
+
+  const goToPrevPage = () =>
+    setPageNumber((prev) => {
+      if (prev - 1 < 1) return prev;
+      return prev - 1;
+    });
+
+  const onZoomIn = () => {
+    const availScales = [1, 1.5, 2, 2.5];
+
+    const oldScaleIndex = availScales.indexOf(scale);
+    if (oldScaleIndex === availScales.length - 1) return;
+    else setScale(availScales[oldScaleIndex + 1]);
+  };
+
+  const onZoomOut = () => {
+    const availScales = [1, 1.5, 2, 2.5];
+
+    const oldScaleIndex = availScales.indexOf(scale);
+    if (oldScaleIndex === 0) return;
+    else setScale(availScales[oldScaleIndex - 1]);
+  };
+
+  const onRotate = () => {
+    setRotation((prev) => prev + 90);
+  };
+
+  const onLoadError = (e: any) => {
+    console.log(e);
+    toast.error("Error loading PDF file", {
+      description: "There was some error loading your pdf file.",
+    });
+  };
+
+  return (
+    <div className="flex-1 flex flex-col gap-0 overflow-x-hidden bg-[#DDD8C2] print:hidden">
+      <div
+        ref={ref}
+        className="flex items-center px-0 justify-between w-full h-14 flex-shrink-0"
+      >
+        <div className="flex items-center gap-1 sm:gap-3 ml-2 sm:ml-6">
+          <Button
+            size={"icon"}
+            variant={"ghost"}
+            onClick={goToPrevPage}
+            disabled={pageNumber === 1}
+          >
+            <ChevronLeft className="w-5 h-5 text-black/70" />
+          </Button>
+
+          <div className="flex items-center gap-1.5">
+            <Input
+              type="number"
+              onBlur={() => {
+                if (Number.isNaN(inputPageNumber))
+                  setInputPageNumber(pageNumber);
+              }}
+              value={inputPageNumber}
+              onChange={(e) => setInputPageNumber(parseInt(e.target.value))}
+              className="w-8 h-6 p-0 text-center text-black [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none bg-transparent border-black/70"
+            />
+            <p className="text-sm text-black">\ {numPages}</p>
+          </div>
+
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            onClick={goToNextPage}
+            disabled={!numPages || pageNumber === numPages}
+          >
+            <ChevronRight className="w-5 h-5 text-black/70" />
+          </Button>
+        </div>
+
+        <div className="flex items-center gap-2 sm:gap-10 mr-2 sm:mr-6">
+          <div className="flex gap-2 sm:gap-4">
+            <Button
+              size={"icon"}
+              variant={"ghost"}
+              onClick={onZoomIn}
+              disabled={scale === 2.5}
+            >
+              <ZoomIn className="sm:w-5 sm:h-5 w-[18px] h-[18px] text-black/70" />
+            </Button>
+
+            <Button
+              size={"icon"}
+              variant={"ghost"}
+              onClick={onZoomOut}
+              disabled={scale === 1}
+            >
+              <ZoomOut className="sm:w-5 sm:h-5 w-[18px] h-[18px] text-black/70" />
+            </Button>
+          </div>
+
+          <Button variant={"ghost"} size={"icon"} onClick={onRotate}>
+            <RotateCcw className="sm:w-5 sm:h-5 w-[18px] h-[18px] text-black/70" />
+          </Button>
+        </div>
+      </div>
+
+      <ScrollArea>
+        <Document
+          file={file}
+          onLoadError={onLoadError}
+          loading={PDFLoadingState}
+          onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+        >
+          <Page
+            pageNumber={pageNumber}
+            width={width ?? 1}
+            rotate={rotation}
+            scale={scale}
+          />
+        </Document>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
+  );
+};
+
+const PDFLoadingState = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center mt-28">
+      <Loader2 className=" animate-spin w-6 h-6 text-brown-600" />
+    </div>
+  );
+};
---
+++ src/app/subjects/[subjectId]/page.tsx
@@ -0,0 +1,113 @@
+import React, { Suspense } from "react";
+
+import Image from "next/image";
+
+import prisma from "@/app/utils/prismadb";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import { SubjectType } from "@/types";
+import { Badge } from "@/components/ui/badge";
+import { Sidebar } from "./_components/Sidebar";
+import { createImageUrlFromWebViewLink } from "@/utils/utils";
+import { Loader, Plus } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default async function SubjectPage({
+  params,
+}: {
+  params: { subjectId: string };
+}) {
+  const subject = (await prisma.subject.findUnique({
+    where: { id: params.subjectId },
+  })) as SubjectType;
+
+  if (!subject) return <p>Invalid Subject Id</p>;
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className="md:max-h-[calc(100vh-89px)] max-h-[calc(100vh-73px)] md:min-h-[calc(100vh-89px)] min-h-[calc(100vh-73px)] flex">
+        <Sidebar subject={subject} />
+
+        <section className="md:flex-1 hidden md:flex items-center flex-col gap-16 overflow-y-auto p-5 select-none">
+          <Breadcrumb className="w-full">
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/blogs" className="sm:text-base">
+                  Subjects
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage className="max-w-56 sm:max-w-80 line-clamp-1 sm:text-base">
+                  {subject.name}
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+
+          <div className="flex flex-col w-full flex-1 items-center justify-center gap-5">
+            <div className="w-full max-w-[420px] rounded-xl bg-zinc-50 hover:bg-white transition-colors border border-zinc-200 p-1.5 flex flex-col gap-2 group cursor-pointer z-20">
+              <div className="w-full bg-white border border-zinc-200 rounded-xl p-1.5">
+                <div className="rounded-xl relative w-full h-full overflow-hidden aspect-w-16 aspect-h-8">
+                  <Image
+                    src={createImageUrlFromWebViewLink(subject.imageUrl)}
+                    alt="subject Cover Image"
+                    className="object-cover group-hover:scale-105 transition-transform"
+                    fill
+                  />
+                </div>
+              </div>
+
+              <div className="p-2 flex flex-col items-end gap-2">
+                <p className="text-base md:text-lg text-zinc-700 font-medium line-clamp-2 w-full text-start h-12">
+                  {subject.name}
+                </p>
+
+                <div className="flex items-center gap-3">
+                  <Badge className="bg-white text-sm text-zinc-600 font-medium border border-zinc-200 rounded-lg hover:bg-white">
+                    {subject.universityShort}
+                  </Badge>
+                  <Badge className="bg-white text-sm text-zinc-600 font-medium border border-zinc-200 rounded-lg hover:bg-white">
+                    Semester {subject.semester}
+                  </Badge>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="rounded-full p-1 border border-zinc-600">
+                <Plus className="text-zinc-600 lg:w-4 lg:h-4 w-3 h-3" />
+              </div>
+              <h2 className="text-lg lg:text-xl font-medium text-gray-600">
+                Open any file.
+              </h2>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Suspense>
+  );
+}
+
+const Loading = () => {
+  return (
+    <div className="md:max-h-[calc(100vh-89px)] max-h-[calc(100vh-73px)] md:min-h-[calc(100vh-89px)] min-h-[calc(100vh-73px)] flex">
+      <Skeleton className="flex max-w-full md:max-w-64 w-full lg:max-w-80 max-md:bg-zinc-100 rounded-none items-center justify-center">
+        <Loader className="text-zinc-600 animate-spin md:hidden" />
+      </Skeleton>
+
+      <div className="flex-1 md:flex items-center justify-center hidden">
+        <Loader className="text-zinc-700 animate-spin" />
+      </div>
+    </div>
+  );
+};
