e03e2a90dcf019a2cb5061606922632e4572c792
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-07-28 20:54:04 +0500

    Revamped the design

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -11,7 +11,7 @@ export const Heading = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="flex flex-col gap-3 items-center z-10">
+    <div className="flex flex-col gap-3 items-center z-10 px-4">
       <h1
         className={cn(
           "text-5xl md:text-6xl font-semibold text-themeSecondary font-brand text-center",
