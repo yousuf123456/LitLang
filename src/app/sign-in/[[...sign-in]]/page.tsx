22fdd23d922326d3ece888e6d057d7b3f92ae91f
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-08-04 20:19:10 +0500

    Some more small changes

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,12 @@
+import { PaddingTopWrapper } from "@/components/PaddingTopWrapper";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <SignIn />
-    </div>
+    <PaddingTopWrapper>
+      <div className="flex w-full h-[calc(100vh-72px)] items-center justify-center">
+        <SignIn />
+      </div>
+    </PaddingTopWrapper>
   );
 }
