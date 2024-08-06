59296f7840ab66b89af5b7cf7fb71f9ba46239cf
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-08-06 11:07:00 +0500

    Logging Enabled

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,12 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isBlogEditorPage = createRouteMatcher(["/blogEditor(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isBlogEditorPage(req)) auth().protect();
-});
+export default clerkMiddleware(
+  (auth, req) => {
+    if (isBlogEditorPage(req)) auth().protect();
+  },
+  { debug: true }
+);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
