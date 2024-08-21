5e400fcc92c875a5a0a4538a8d8fa5b2c53c5acd
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-08-21 11:20:32 +0500

    PDF CHAT BOT INTRODUCED (Fixing)

--- src/app/subjects/[subjectId]/[fileId]/_components/AiChat/ChatContext.tsx
+++ src/app/subjects/[subjectId]/[fileId]/_components/AiChat/ChatContext.tsx
@@ -83,80 +83,46 @@ export const ChatContext = ({
       return response;
     },
     onMutate: async (newMessage) => {
-      await utils.chat.getMessagePages.cancel();
-
-      const previousMessages = utils.chat.getMessagePages.getInfiniteData({
-        fileId,
-      });
-
-      console.log("Adding new message");
-
       utils.chat.getMessagePages.setInfiniteData(
         { fileId, limit: 1 },
         (oldData) => {
           if (!oldData) return oldData;
 
-          const oldPages = oldData.pages;
-          const newPages = [...oldPages];
-
-          let latestPage = newPages[0];
-
-          // const newUserMessage: ExtendedMessageType = {
-          //   createdAt: new Date().toString(),
-          //   id: ObjectID().toHexString(),
-          //   isUserMessage: true,
-          //   gotRejected: false,
-          //   text: newMessage,
-          // };
-
-          // const newAiMessage: ExtendedMessageType = {
-          //   id: "ai-streaming-response",
-          //   isAiWriting: true,
-          //   gotRejected: false,
-          //   isAiThinking: true,
-          //   isUserMessage: false,
-          //   createdAt: new Date().toString(),
-          //   text: "Litera is processing your query...",
-          // };
-
-          latestPage.messages = [
-            {
-              createdAt: new Date().toString(),
-              id: ObjectID().toHexString(),
-              isUserMessage: true,
-              gotRejected: false,
-              text: newMessage,
-            },
-            {
-              id: "ai-streaming-response",
-              isAiWriting: true,
-              gotRejected: false,
-              isAiThinking: true,
-              isUserMessage: false,
-              createdAt: new Date().toString(),
-              text: "Litera is processing your query...",
-            },
-            ...latestPage.messages,
-          ];
-
-          // latestPage.messages.unshift(newUserMessage);
-          // latestPage.messages.unshift(newAiMessage);
-
-          newPages[0] = latestPage;
-
-          return {
-            pages: newPages,
-            pageParams: oldData.pageParams,
+          const newUserMessage: ExtendedMessageType = {
+            createdAt: new Date().toString(),
+            id: ObjectID().toHexString(),
+            isUserMessage: true,
+            gotRejected: false,
+            text: newMessage,
           };
+
+          const newAiMessage: ExtendedMessageType = {
+            id: "ai-streaming-response",
+            isAiWriting: true,
+            gotRejected: false,
+            isAiThinking: true,
+            isUserMessage: false,
+            createdAt: new Date().toString(),
+            text: "Litera is processing your query...",
+          };
+
+          const updatedPages = oldData.pages.map((page, i) => {
+            if (i === 0) {
+              const updatedMessages = [
+                newAiMessage,
+                newUserMessage,
+                ...page.messages,
+              ];
+
+              return { ...page, messages: updatedMessages };
+            } else return page;
+          });
+
+          return { ...oldData, pages: updatedPages };
         }
       );
 
       setIsLoading(true);
-
-      return {
-        prevMessages:
-          previousMessages?.pages.flatMap((page) => page.messages) ?? [],
-      };
     },
     onSuccess: async (response) => {
       if (!response.body)
