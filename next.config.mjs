276de7fe984530757a1a02f91f9bde722f050050
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-11-13 14:29:24 +0500

    Upgraded Nextjs to latest version (Fixing Errors)

--- next.config.mjs
+++ next.config.mjs
@@ -32,6 +32,12 @@ const nextConfig = {
 
   //   return config;
   // },
+  future: { webpack5: true },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 export default nextConfig;
