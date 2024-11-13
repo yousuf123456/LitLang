5809198a25392a8145bea819b6134fb726445501
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-11-13 14:25:38 +0500

    Upgraded Nextjs to latest version (Fixing Errors)

--- next.config.mjs
+++ next.config.mjs
@@ -11,27 +11,27 @@ const nextConfig = {
       { hostname: "utfs.io" },
     ],
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
-  },
-  webpack: (config, { isServer, nextRuntime }) => {
-    if (true) {
-      config.snapshot.managedPaths = [];
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   config.resolve.alias.canvas = false;
+  //   config.resolve.alias.encoding = false;
+  //   return config;
+  // },
+  // webpack: (config, { isServer, nextRuntime }) => {
+  //   if (true) {
+  //     config.snapshot.managedPaths = [];
 
-      Object.defineProperty(config, "watchOptions", {
-        ...Object.getOwnPropertyDescriptor(config, "watchOptions"),
-        value: {
-          ...config.watchOptions,
-          ignored:
-            /^((?:[^/]*(?:\/|$))*)(\.(git|next))(\/((?:[^/]*(?:\/|$))*)(?:$|\/))?/,
-        },
-      });
-    }
+  //     Object.defineProperty(config, "watchOptions", {
+  //       ...Object.getOwnPropertyDescriptor(config, "watchOptions"),
+  //       value: {
+  //         ...config.watchOptions,
+  //         ignored:
+  //           /^((?:[^/]*(?:\/|$))*)(\.(git|next))(\/((?:[^/]*(?:\/|$))*)(?:$|\/))?/,
+  //       },
+  //     });
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 export default nextConfig;
